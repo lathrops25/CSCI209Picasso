1e92802fc0bce4a49f07e42e550f1ecd8f2a382f
Sarah Lathrop <lathrops25@mail.wlu.edu>
2024-11-18 21:39:56 -0500

    Added test for expression tree generator for absolute value

--- src/tests/ExpressionTreeGeneratorTests.java
+++ src/tests/ExpressionTreeGeneratorTests.java
@@ -90,5 +90,14 @@ public class ExpressionTreeGeneratorTests {
 		e = parser.makeExpression("floor( x + y )");
 		assertEquals(new Floor(new Addition(new X(), new Y())), e);
 	}
+	
+	@Test
+	public void absFunctionTests() {
+		ExpressionTreeNode e = parser.makeExpression("abs( x )");
+		assertEquals(new Abs(new X()), e);
+		
+		e = parser.makeExpression("abs( x + y )");
+		assertEquals(new Abs(new Addition(new X(), new Y())), e);
+	}
 
 }

433fff32cf4ee492b2f004d6cb7f55ad72c9ff15
Jonathan <carranzacortesj25@mail.wlu.edu>
2024-11-20 16:00:38 -0500

    Added text field, works with enter or evaluate button

--- src/picasso/view/Frame.java
+++ src/picasso/view/Frame.java
@@ -2,7 +2,12 @@ package picasso.view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 import picasso.model.Pixmap;
 import picasso.util.ThreadedCommand;
@@ -12,31 +17,49 @@ import picasso.view.commands.*;
  * Main container for the Picasso application
  *
  * @author Robert Duvall (rcd@cs.duke.edu)
- * 
+ * @author Jonathan Carranza Cortes
  */
 @SuppressWarnings("serial")
 public class Frame extends JFrame {
+
+	private JTextField textField;
+	private Evaluator eval;
+
 	public Frame(Dimension size) {
+
+
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		// create GUI components
 		Canvas canvas = new Canvas(this);
 		canvas.setSize(size);
 		setTitle("CodeCatalysts");
+		
+		// create an input text field
+		textField = new JTextField(40);
+		eval = new Evaluator(textField);
 
 		// add commands to test here
 		ButtonPanel commands = new ButtonPanel(canvas);
 		commands.add("Open", new Reader());
-		commands.add("Evaluate", new ThreadedCommand<Pixmap>(canvas, new Evaluator()));
+		commands.add("Evaluate", new ThreadedCommand<Pixmap>(canvas, eval));
 		commands.add("Save", new Writer());
 		
-		// adding separate text box
-		TextBox inputTextBox = new TextBox();
-		inputTextBox.setVisible(true);
-
+		// evaluate when pressing enter
+		textField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				// getting evaluate button to execute
+				JButton evalButton = new JButton();
+				evalButton = commands.getButton(1);
+				evalButton.doClick();	// simulates clicking the evaluate button
+			}
+		});
+		
 		// add our container to Frame and show it
-		getContentPane().add(canvas, BorderLayout.CENTER);
-		getContentPane().add(commands, BorderLayout.NORTH);
+		getContentPane().add(canvas, BorderLayout.SOUTH);
+		getContentPane().add(commands, BorderLayout.CENTER);
+		getContentPane().add(textField, BorderLayout.NORTH);
 		pack();
 	}
+	
 }

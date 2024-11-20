c61f38789f391c6eeb1af5fcf2bf799c49db29a9
Jonathan <carranzacortesj25@mail.wlu.edu>
2024-11-20 16:01:09 -0500

    Added new constructor, takes in a TextField

--- src/picasso/view/commands/Evaluator.java
+++ src/picasso/view/commands/Evaluator.java
@@ -3,25 +3,38 @@ package picasso.view.commands;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.io.Console;
+
+import javax.swing.JTextField;
 
 import picasso.model.Pixmap;
 import picasso.parser.ExpressionTreeGenerator;
 import picasso.parser.language.ExpressionTreeNode;
 import picasso.util.Command;
-import picasso.view.Canvas;
 
 /**
  * Evaluate an expression for each pixel in a image.
  * 
  * @author Robert C Duvall
  * @author Sara Sprenkle
+ * @author Jonathan Carranza Cortes
  */
 public class Evaluator implements Command<Pixmap> {
+	
 	public static final double DOMAIN_MIN = -1;
 	public static final double DOMAIN_MAX = 1;
-	public static String inputString;
+	
+	private static String inputString;
+	private JTextField textIn;
 
+	
+	/**
+	 * Constructor that takes input from text field and saves it
+	 * @param text field
+	 */
+	public Evaluator(JTextField textIn) {
+		this.textIn = textIn;
+	}
+	
 	/**
 	 * Evaluate an expression for each point in the image.
 	 */
@@ -49,30 +62,18 @@ public class Evaluator implements Command<Pixmap> {
 	}
 
 	/**
-	 * 
-	 * A place holder for a more interesting way to build the expression.
+	 * Creates expression based on text in a text field
 	 */
 	private ExpressionTreeNode createExpression() {
 		// Note, when you're testing, you can use the ExpressionTreeGenerator to
 		// generate expression trees from strings, or you can create expression
 		// objects directly (as in the commented statement below).
 
-//		String inputString = "floor(y)";
-		//String test = "x + y";
-		
+		// Take the current expression in the text field
+		inputString = textIn.getText();
 
 		ExpressionTreeGenerator expTreeGen = new ExpressionTreeGenerator();
 		return expTreeGen.makeExpression(inputString);
-
-		// return new Multiply( new X(), new Y() );
 	}
-	/**
-	 * Setter method to receive input from the other GUI
-	 * and set it.
-	 * @param newString
-	 */
-	public void receiveInputString(String newString) {
-		inputString = newString;
-	}
-
+	
 }

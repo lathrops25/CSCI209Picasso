b43032ac6add5ddd47144b76740b903c05469a4c
allisonhidalgo <hidalgoa25@mail.wlu.edu>
2024-11-20 19:36:02 -0500

    Clamp Analyzer,Token, and Test

---
+++ src/picasso/parser/tokens/functions/ClampToken.java
@@ -0,0 +1,15 @@
+package picasso.parser.tokens.functions;
+
+/**
+ * This represents the clamp function token 
+ * 
+ * @author Allison Hidalgo
+ */
+public class ClampToken extends FunctionToken {
+
+	public ClampToken() {
+		super("Clamp Function Token");
+	}
+
+}
+

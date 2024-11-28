ef33ec125224875256d729fefd484f957fbd67c9
Carzec <127870306+Carzec@users.noreply.github.com>
2024-11-28 03:08:06 -0600

    Added method to check if a string has a file extension

--- src/picasso/parser/tokens/IdentifierToken.java
+++ src/picasso/parser/tokens/IdentifierToken.java
@@ -10,11 +10,15 @@ package picasso.parser.tokens;
 public class IdentifierToken extends Token {
 
 	private final String myName;
+	// Used to check if string is an image
+	private String[] validExtensions = {"jpg", "png"};
+    private String fileEx;
 
 	public IdentifierToken(String value) {
 		super("Variable Token");
 		myName = value;
 	}
+	
 
 	public boolean equals(Object o) {
 		if( o == this ) {
@@ -46,6 +50,25 @@ public class IdentifierToken extends Token {
 
 	@Override
 	public boolean isFunction() {
+		return false;
+	}
+	
+	/**
+	 * checks for a file extension
+	 * used to check if something is an image
+	 * @return true
+	 */
+	public boolean fileExtension() {
+	    int lastPeriod;
+	    String fileEx;
+	    lastPeriod = myName.lastIndexOf(".");
+	    fileEx = myName.substring(lastPeriod);
+	    
+	    for (String s: validExtensions) {
+	    	if (s.equals(fileEx) ){ 
+	    		return true;  
+	    			}
+	    }
 		return false;
 	}
 

4ba2d494409f9c3ba2094c300e08a73698084c0e
Sarah Lathrop <lathrops25@mail.wlu.edu>
2024-11-29 18:12:36 -0500

    Edited evaluate, added equals method and compareTo (for map)

--- src/picasso/parser/language/expressions/Variable.java
+++ src/picasso/parser/language/expressions/Variable.java
@@ -5,10 +5,11 @@ import picasso.parser.language.ExpressionTreeNode;
 /**
  * Represents a variable
  * 
- * @author Sara Sprenkle
+ * @author Sara Sprenkle, Sarah Lathrop
+ * 
  *
  */
-public class Variable extends ExpressionTreeNode {
+public class Variable extends ExpressionTreeNode implements Comparable<Variable>  {
 
 	private String name;
 
@@ -20,11 +21,44 @@ public class Variable extends ExpressionTreeNode {
 	public RGBColor evaluate(double x, double y) {
 		// TODO Auto-generated method stub
 		// Should be set to some value using assignment.
-		return null;
+		ExpressionTreeNode expression = gelementsToValue.get(this);
+		RGBColor value = expression.evaluate(x, y);
+		//evaluate expression
+		return value;
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+
+		if (!(o instanceof Variable)) {
+			return false;
+		}
+
+		// Make sure the objects are the same type
+
+		if (o.getClass() != this.getClass()) {
+			return false;
+		}
+
+		Variable uf = (Variable) o;
+
+		// check if their parameters are equal
+		if (!this.name.equals(uf.name)) {
+			return false;
+		}
+		return true;
+	}
+	
 	public String getName() {
 		return name;
+	}
+	
+	@Override
+	public int compareTo(Variable other) {
+        return name.compareTo(other.getName());
 	}
 
 }

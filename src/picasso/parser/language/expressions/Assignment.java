2a29c0cc4f5c6bdac78da4af8fae7f9f59ecefe0
Sarah Lathrop <lathrops25@mail.wlu.edu>
2024-11-29 18:10:50 -0500

    Wrote the assignment operator

---
+++ src/picasso/parser/language/expressions/Assignment.java
@@ -0,0 +1,66 @@
+package picasso.parser.language.expressions;
+import picasso.parser.language.ExpressionTreeNode;
+
+/**
+ * Represents the Assignment operator
+ * 
+ * @author Sarah Lathrop
+ */
+public class Assignment extends ExpressionTreeNode {
+	Variable left;
+	ExpressionTreeNode right;
+	
+	/**
+	 * Constructor 
+	 * @param left
+	 * @param right
+	 */
+	public Assignment (Variable left, ExpressionTreeNode right) {
+		this.left = left;
+		this.right = right;
+		//map the variable to right (don't map the evaluation)
+		gelementsToValue.put(left, right);
+	}
+	
+	@Override
+	public RGBColor evaluate (double x, double y) {
+		// get the name of the variable and map this to the variable in the idToExpression map
+		RGBColor result = right.evaluate(x, y);
+		return result;
+	}
+		
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+
+		if (!(o instanceof Assignment)) {
+			return false;
+		}
+
+		// Make sure the objects are the same type
+
+		if (o.getClass() != this.getClass()) {
+			return false;
+		}
+
+		Assignment uf = (Assignment) o;
+
+		// check if their parameters are equal
+		if (!this.left.equals(uf.left)) {
+			return false;
+		}
+		if (!this.right.equals(uf.right)) {
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		String classname = this.getClass().getName();
+		return classname.substring(classname.lastIndexOf(".") + 1) + "(" + left + "=" + right + ")";
+	}
+
+}

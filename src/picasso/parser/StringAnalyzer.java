ee955a5b390b5e1dee43dc2686714de0884d8590
Carzec <127870306+Carzec@users.noreply.github.com>
2024-12-02 15:40:17 -0500

    Created Analyzer for StringToken

---
+++ src/picasso/parser/StringAnalyzer.java
@@ -0,0 +1,44 @@
+package picasso.parser;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.StringNode;
+import picasso.parser.language.expressions.Variable;
+import picasso.parser.language.expressions.X;
+import picasso.parser.language.expressions.Y;
+import picasso.parser.tokens.StringToken;
+import picasso.parser.tokens.Token;
+
+/**
+ *  Handles parsing the quotations
+ */
+public class StringAnalyzer implements SemanticAnalyzerInterface{
+	
+	static Map<String, ExpressionTreeNode> idToExpression = new HashMap<String, ExpressionTreeNode>();
+
+	static {
+		// We always have x and y defined.
+		idToExpression.put("x", new X());
+		idToExpression.put("y", new Y());
+	}
+
+	
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		StringToken t = (StringToken) tokens.pop();
+		String id = t.getName();
+		ExpressionTreeNode mapped = idToExpression.get(id);
+		if (mapped == null) {
+			idToExpression.put(id, new Variable (id));
+			mapped = idToExpression.get(id);
+		}
+
+		// TODO : What should we do if we don't recognize the identifier?
+		// Is that an error? Or, could there a valid reason?
+		
+		return new StringNode(id);
+	}
+}

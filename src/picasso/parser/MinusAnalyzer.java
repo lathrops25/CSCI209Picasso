319394b46fc5bbf2d352b0ad409bc628c82e1332
Sarah Lathrop <lathrops25@mail.wlu.edu>
2024-12-04 20:41:45 -0500

    Minus Analyzer

---
+++ src/picasso/parser/MinusAnalyzer.java
@@ -0,0 +1,31 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Addition;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing the minus or "subtraction function".
+ * 
+ * @author Sarah Lathrop
+ * 
+ */
+public class MinusAnalyzer implements SemanticAnalyzerInterface {
+
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Remove the plus token
+		// the parameters are the next tokens on the stack.
+		// But, they need to be processed
+		
+		ExpressionTreeNode paramETNRight = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		ExpressionTreeNode paramETNLeft = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		
+		return new Subtraction(paramETNLeft, paramETNRight);
+	}
+
+}

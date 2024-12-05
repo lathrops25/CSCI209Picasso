860634855a0e1396e2d73bf90fa86ff544050ac1
Sarah Lathrop <lathrops25@mail.wlu.edu>
2024-12-04 20:45:26 -0500

    Comment edits

--- src/picasso/parser/MinusAnalyzer.java
+++ src/picasso/parser/MinusAnalyzer.java
@@ -16,7 +16,7 @@ public class MinusAnalyzer implements SemanticAnalyzerInterface {
 
 	@Override
 	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
-		tokens.pop(); // Remove the plus token
+		tokens.pop(); // Remove the subtraction token
 		// the parameters are the next tokens on the stack.
 		// But, they need to be processed
 		

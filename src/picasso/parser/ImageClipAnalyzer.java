096021c31b496b6908edfda4166b16240947a882
Carzec <127870306+Carzec@users.noreply.github.com>
2024-12-06 10:35:37 -0500

    handles imageClip parsing

---
+++ src/picasso/parser/ImageClipAnalyzer.java
@@ -0,0 +1,44 @@
+package picasso.parser;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.ImageClip;
+import picasso.parser.tokens.StringToken;
+import picasso.parser.tokens.Token;
+
+/**
+ * Handles parsing for ImageClip
+ * @author Jonathan
+ */
+public class ImageClipAnalyzer extends UnaryFunctionAnalyzer{
+static Map<String, ExpressionTreeNode> idToExpression = new HashMap<String, ExpressionTreeNode>();
+
+	
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Need to remove the imageClip token
+		// the parameters are the next token(s) on the stack.
+		// But, it needs to be processed
+		
+		// deal with y-expression
+		ExpressionTreeNode paramETN1 = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		
+		// deal with x-expression
+		ExpressionTreeNode paramETN2 = SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens);
+		
+		// final pop off to deal with string, last should be string token
+		StringToken t = (StringToken) tokens.pop();
+		String id = t.getName();
+		ExpressionTreeNode mapped = idToExpression.get(id);
+		if (mapped != null) {
+			return mapped;
+		}
+		
+		
+		return new ImageClip(paramETN1, paramETN2, id);
+	}
+}

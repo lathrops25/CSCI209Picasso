618d6ecdc1c799417a78852044721ae628fb1219
Sarah Lathrop <lathrops25@mail.wlu.edu>
2024-12-07 12:42:42 -0500

    Order of operations with Minus

--- src/tests/OrderofOperationsTest.java
+++ src/tests/OrderofOperationsTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import picasso.parser.ExpressionTreeGenerator;
 import picasso.parser.language.ExpressionTreeNode;
-import picasso.parser.language.expressions.Addition;
-import picasso.parser.language.expressions.Multiplication;
-import picasso.parser.language.expressions.X;
+import picasso.parser.language.expressions.*;
 
 /**
  * Tests for verifying order of operations in the ExpressionTreeGenerator.
@@ -36,5 +34,29 @@ public class OrderofOperationsTest {
         // Test for "x * (x + x)" -> x * (x + x)
         ExpressionTreeNode e = parser.makeExpression("x * (x + x)");
         assertEquals(new Multiplication(new X(), new Addition(new X(), new X())), e);
+    }
+    
+    @Test 
+    public void testSubandAdd() {
+    	//Test for x - y + x  -> (x-y) + x
+    	ExpressionTreeNode e = parser.makeExpression("x - y + x");
+    	assertEquals(new Addition(new Subtraction (new X(), new Y()), new X()), e);
+    	
+    	//Test for x + y - x -> (x+y) - x
+    	ExpressionTreeNode e2 = parser.makeExpression("x + y - x");
+    	assertEquals(new Subtraction(new Addition (new X(), new Y()), new X()), e2);
+    }
+    
+    @Test
+    public void testMultiplicationAndSubtractionPrecedence() {
+        // Test for "x * x - x" -> (x * x) - x
+        ExpressionTreeNode e = parser.makeExpression("x * x - x");
+        assertEquals(new Subtraction(new Multiplication(new X(), new X()), new X()), e);
+    }
+    
+    public void testParenthesesPrecedenceSub() {
+        // Test for "x * (x - x)" -> x * (x - x)
+        ExpressionTreeNode e = parser.makeExpression("x * (x - x)");
+        assertEquals(new Multiplication(new X(), new Subtraction(new X(), new X())), e);
     }
 }

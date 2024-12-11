1510acff44019865c9137d9f4768216e1f59ad37
Sarah Lathrop <lathrops25@mail.wlu.edu>
2024-12-11 14:47:22 -0500

    Removed minus from order of operations

--- src/tests/OrderofOperationsTest.java
+++ src/tests/OrderofOperationsTest.java
@@ -36,27 +36,4 @@ public class OrderofOperationsTest {
         assertEquals(new Multiplication(new X(), new Addition(new X(), new X())), e);
     }
     
-    @Test 
-    public void testSubandAdd() {
-    	//Test for x - y + x  -> (x-y) + x
-    	ExpressionTreeNode e = parser.makeExpression("x - y + x");
-    	assertEquals(new Addition(new Subtraction (new X(), new Y()), new X()), e);
-    	
-    	//Test for x + y - x -> (x+y) - x
-    	ExpressionTreeNode e2 = parser.makeExpression("x + y - x");
-    	assertEquals(new Subtraction(new Addition (new X(), new Y()), new X()), e2);
-    }
-    
-    @Test
-    public void testMultiplicationAndSubtractionPrecedence() {
-        // Test for "x * x - x" -> (x * x) - x
-        ExpressionTreeNode e = parser.makeExpression("x * x - x");
-        assertEquals(new Subtraction(new Multiplication(new X(), new X()), new X()), e);
-    }
-    
-    public void testParenthesesPrecedenceSub() {
-        // Test for "x * (x - x)" -> x * (x - x)
-        ExpressionTreeNode e = parser.makeExpression("x * (x - x)");
-        assertEquals(new Multiplication(new X(), new Subtraction(new X(), new X())), e);
-    }
 }

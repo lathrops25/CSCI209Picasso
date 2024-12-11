8ad5a09f7b1097140b5690589417da25590480a1
Sarah Lathrop <lathrops25@mail.wlu.edu>
2024-12-11 12:31:09 -0500

    Tests for division

--- src/tests/SemanticAnalyzerTest.java
+++ src/tests/SemanticAnalyzerTest.java
@@ -195,4 +195,20 @@ class SemanticAnalyzerTest {
 			semAnalyzer.generateExpressionTree(tokens);
 		});
 	}
+	
+	@Test
+	void testParseDivision() {
+
+	    // Set up the token stack for "x * y"
+	    Stack<Token> tokens = new Stack<>();
+	    tokens.push(new IdentifierToken("x")); // Push the left operand
+	    tokens.push(new IdentifierToken("y")); // Push the right operand
+	    tokens.push(new DivisionToken());     // Push the multiplication operator
+
+	    // Generate the expression tree
+	    ExpressionTreeNode actual = semAnalyzer.generateExpressionTree(tokens);
+
+	    // Expected: Multiplication(x, y)
+	    assertEquals(new Division(new X(), new Y()), actual);
+	}
 }

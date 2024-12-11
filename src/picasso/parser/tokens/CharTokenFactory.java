c30e189b506286ccaa4c48ecbbc58aceb384e836
Sarah Lathrop <lathrops25@mail.wlu.edu>
2024-12-11 12:31:01 -0500

    Implemented division

--- src/picasso/parser/tokens/CharTokenFactory.java
+++ src/picasso/parser/tokens/CharTokenFactory.java
@@ -39,6 +39,7 @@ public class CharTokenFactory {
 		tokenList[CharConstants.PLUS] = new PlusToken();
 		tokenList[CharConstants.QUOTE] = new QuoteToken();
 		tokenList[CharConstants.STAR] = new MultiplicationToken();
+		tokenList[CharConstants.SLASH] = new DivisionToken();
 
 		// TODO: What other tokens should be in here?
 		// TODO: Need to create the associated Token classes too.
